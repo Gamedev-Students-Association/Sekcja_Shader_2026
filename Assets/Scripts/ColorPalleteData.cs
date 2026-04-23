e84cabd1aa6a4a9d211289987d2a78bb864299f4
MagicalMatrix <magical0matrix@gmail.com>
2026-04-23 17:37:46 +0200

    color pallete single color output

---
+++ Assets/Scripts/ColorPalleteData.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColorPalleteData : MonoBehaviour
+{
+    [SerializeField]
+    protected Material material;
+
+    protected int colorsPropertyID;
+    protected int colorsSizePropertyID;
+
+    [SerializeField]
+    protected Color[] colors;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorsPropertyID = Shader.PropertyToID("_ColorSet");
+        colorsSizePropertyID = Shader.PropertyToID("_ColorSetSize");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.SetColorArray(colorsPropertyID, colors);
+        material.SetInteger(colorsSizePropertyID, colors.Length);
+    }
+}
